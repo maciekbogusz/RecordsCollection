12f3f2dd8ee33721085f96d63b046f6b68ffdf35
Maciej Bogusz <maciekbogusz@yahoo.com>
2018-03-02 16:39:00 +0100

    - new project

---
+++ RecordsCollection/index.js
@@ -0,0 +1,5 @@
+function showAlert(artistId, albumId){ 
+		var tempArtists = document.getElementById(artistId).value;
+		var tempAlbum = document.getElementById(albumId).value;
+		alert(tempArtists+" "+tempAlbum);			
+}
