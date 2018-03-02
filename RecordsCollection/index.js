90a0dcdecd61f405594e92ae360338293af66901
Maciek <maciekbogusz@yahoo.com>
2018-03-02 22:29:33 +0100

    - added condition to not show empty dialog

--- RecordsCollection/index.js
+++ RecordsCollection/index.js
@@ -1,5 +1,19 @@
 function showAlert(artistId, albumId){ 
 		var tempArtists = document.getElementById(artistId).value;
 		var tempAlbum = document.getElementById(albumId).value;
-		alert(tempArtists+" "+tempAlbum);			
+		if (!!tempArtists & !!tempAlbum){
+			alert(tempArtists+" "+tempAlbum);
+		}					
 }
+
+
+
+
+function createClient(){
+	var client = amazon.createClient({
+	awsId: "aws ID",
+	awsSecret: "aws Secret",
+	awsTag: "aws Tag"
+});
+}
+
